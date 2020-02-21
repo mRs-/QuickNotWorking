315d275ca9eff7a9850f5bb3521b74627bab2d17
Marius Landwehr <marius@deepl.com>
2020-02-21 11:38:35 +0100

    Initial Commit

---
+++ QuickNotWorking/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QuickNotWorking
+//
+//  Created by Marius Landwehr on 21.02.20.
+//  Copyright © 2020 Marius Landwehr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ QuickNotWorkingTests/QuickNotWorkingTests.swift
@@ -0,0 +1,15 @@
+@testable import QuickNotWorking
+import Quick
+import Nimble
+
+class QuickNotWorkingTests: QuickSpec {
+
+    override func spec() {
+        describe("DummyTests") {
+            describe("Output") {
+                expect(true).to(equal(true))
+            }
+        }
+    }
+
+}
